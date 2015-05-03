644718404e8785fd02a528b03e0c164e441312ca
Wandol <seok45861@gmail.com>
2015-05-03 20:14:21 +0900

    자바 공유 연

---
+++ javaex/src/com/java/ex/TestEx.java
@@ -0,0 +1,20 @@
+package com.java.ex;
+
+public class TestEx {
+		public static void main(String[] args) {
+			int i = 10;
+			int j = 20;
+			int h = 30;
+			
+			char c = 'a';
+			char b = 'b';
+			char g = 'w';
+			
+			System.out.println(i);
+			System.out.println(j);
+			System.out.println(h);
+			System.out.println(h);
+			System.out.println(h);
+			System.out.println(h);
+		}
+}
